4e45a407891af51c1a396999c3daa49f5e466912
Irfan Ahmad <76489214+Irfan-Ahmad-byte@users.noreply.github.com>
2023-04-15 22:16:16 +0500

    Add files via upload

---
+++ chatbot_loader.js
@@ -0,0 +1,19 @@
+(function() {
+  const chatbotIframe = document.createElement('iframe');
+  chatbotIframe.src = 'https://irfan-ahmad.com/chatbot/chatbot.html';
+  chatbotIframe.style.width = '100%';
+  chatbotIframe.style.height = '100%';
+  chatbotIframe.style.border = 'none';
+
+  const chatbotContainer = document.createElement('div');
+  chatbotContainer.style.position = 'fixed';
+  chatbotContainer.style.bottom = '0';
+  chatbotContainer.style.right = '0';
+  chatbotContainer.style.width = '300px';
+  chatbotContainer.style.height = '500px';
+  chatbotContainer.style.zIndex = '10000';
+  chatbotContainer.appendChild(chatbotIframe);
+
+  document.body.appendChild(chatbotContainer);
+})();
+
